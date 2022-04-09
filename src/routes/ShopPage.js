03bc14e82e52ae554b8358c0e4efe45762dac071
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-04-09 11:57:34 +0800

    feat(shop): create an area for the categories pane
    This commit updates the layout of the shop page by adding a categories
    pane in the left-hand side
    
    The categories pane is still empty though, and will be worked on later
    
    This commit also does a little bit of refactoring and code-cleanup:
    
        - The loading screen layout has been modified a bit and the scroll-bar
          is now focused on the products area instead of the whole page

--- src/routes/ShopPage.js
+++ src/routes/ShopPage.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import dancingPepe from "../assets/sad-dancing-pepe.gif";
 
 const ShopPage = () => {
@@ -18,37 +17,51 @@ const ShopPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-wrap items-center justify-center h-full gap-8">
+    <>
 
-      {
+      { // Loading screen component
         products.length === 0 &&
-          <div className="grid place-items-center">
+          <div className="flex flex-col items-center justify-center flex-1 border">
             <p>** are you entertained yet? **</p>
-            <img src={dancingPepe} alt="sad dancing pepe" className="w-[50%]"/>
+            <img src={dancingPepe} alt="sad dancing pepe" className="h-[350px]" />
             <p>Loading Shop...</p>
           </div>
       }
 
-      {products.map(product => {
-        return(
-          <div key={product.id} className="flex flex-col border-2 rounded-xl w-[350px] h-[550px] p-5">
+      { // Main shop page component
+        products.length > 0 &&
+          <div className="flex flex-1 overflow-scroll">
 
-            <div className="w-[300px] h-[300px] p-5 grid place-items-center">
-              <img src={product.image} alt={product.title} className="max-w-full max-h-full"/>
+            <div className="w-1/4 h-full pt-5 text-3xl">
+              <p className="text-center">Categories</p>
             </div>
-            <p>{product.title}</p>
 
-            <p className="mt-auto text-xl font-bold">${product.price}</p>
-            <p>Rating: {product.rating.rate} out of 5</p>
-            <p className="mb-5">Rated by {product.rating.count} customers</p>
-            <button className="w-full px-5 py-1 mb-1 text-xs rounded-full bg-amber-300 hover:bg-amber-300/90 active:bg-amber-300">Add to Cart</button>
-            <button className="w-full px-5 py-1 text-xs rounded-full bg-amber-500 hover:bg-amber-500/90 active:bg-amber-500">Buy Now</button>
+            <div className="flex flex-wrap items-center justify-center w-3/4 h-full pt-10 overflow-scroll border-l gap-8">
 
-          </div>
-        );
-      })}
+                {
+                  products.map(product => {
+                    return(
+                      <div key={product.id} className="flex flex-col border-2 rounded-xl w-[350px] h-[575px] p-5">
+                        <div className="w-[300px] h-[300px] p-5 grid place-items-center">
+                          <img src={product.image} alt={product.title} className="max-w-full max-h-full"/>
+                        </div>
+                        <p>{product.title}</p>
 
-    </div>
+                        <p className="mt-auto text-xl font-bold">${product.price}</p>
+                        <p>Rating: {product.rating.rate} out of 5</p>
+                        <p className="mb-5">Rated by {product.rating.count} customers</p>
+                        <button className="w-full px-5 py-1 mb-1 text-xs rounded bg-amber-300 hover:bg-amber-300/90 active:bg-amber-300">Add to Cart</button>
+                        <button className="w-full px-5 py-1 text-xs rounded bg-amber-500 hover:bg-amber-500/90 active:bg-amber-500">Buy Now</button>
+                      </div>
+                    );
+                  })
+                }
+
+            </div>
+        </div>
+      }
+
+    </>
   );
 
 }
