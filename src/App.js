6110415465c6f0fc4fa54202780b4434c87883d5
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-04-09 11:49:55 +0800

    refactor: change navbar position to sticky
    After going through the tailwindcss docs, I realized that that behavior
    that I want for the navbar is sticky and not fixed
    
    A sticky position is basically a relative position by default but
    changes to absolute positioning after a certain threshold
    
    This makes is so that I don't have to apply unnecessary top padding just
    to ensure that the content below the navbar get seen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { Outlet, Link } from 'react-router-dom';
-
 import logo from './assets/pepe.png';
 
 const App = () => {
 
   return (
-    <div className="h-screen pt-44">
+    <div className="flex flex-col h-screen">
 
-      <header className="fixed top-0 flex items-center w-full p-5 bg-white gap-4 drop-shadow-xl">
+      {/* Navbar component */}
+      <header className="sticky top-0 flex items-center w-full p-5 bg-white gap-4 drop-shadow-xl">
         <img src={logo} alt="pepe the frog logo" className="w-[100px]"/>
-
         <h1 className="text-5xl cursor-default select-none">pepe's SHOP</h1>
-
         <nav className="ml-auto mr-8">
           <ul className="flex items-center justify-center gap-8">
             <Link to="/" className="text-4xl hover:underline">Home</Link>
@@ -21,6 +19,7 @@ const App = () => {
         </nav>
       </header>
 
+      {/* This is where the pages will be displayed */}
       <Outlet />
 
     </div>
