730cd89115cada91a7e3fa3459ddfdc1c5ccce70
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-04-11 11:26:37 +0800

    feat(shop): add categories to the categories pane
    This commit adds code that queries fakestoreapi.com for their product
    categories
    
    These categories are then turned into Links which, on click, filters the
    products by the chosen category

--- src/routes/ShopPage.js
+++ src/routes/ShopPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 import dancingPepe from "../assets/sad-dancing-pepe.gif";
 
 const ShopPage = () => {
@@ -6,14 +7,23 @@ const ShopPage = () => {
   async function fetchProducts() {
     const response = await fetch("https://fakestoreapi.com/products");
     const json = await response.json();
-    console.log(json);
+    return json;
+  }
+
+  async function fetchCategories() {
+    const response = await fetch("https://fakestoreapi.com/products/categories");
+    const json = await response.json();
     return json;
   }
 
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  let [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     fetchProducts().then(json => setProducts(json));
+    fetchCategories().then(json => setCategories(json));
   }, []);
 
   return (
@@ -32,14 +42,29 @@ const ShopPage = () => {
         products.length > 0 &&
           <div className="flex flex-1 overflow-scroll">
 
-            <div className="w-1/4 h-full pt-5 text-3xl">
-              <p className="text-center">Categories</p>
+            <div className="w-1/4 h-full pt-10">
+              <p className="text-3xl leading-loose text-center">Categories</p>
+              <nav>
+                {
+                  categories.map(category => (
+                    <Link to={`?category=${category}`} className="block text-xl text-center" key={category}>
+                      {category}
+                    </Link>
+                  ))
+                }
+              </nav>
             </div>
 
-            <div className="flex flex-wrap items-center justify-center w-3/4 h-full pt-10 overflow-scroll border-l gap-8">
+            <div id="product-grid" className="flex flex-wrap items-center justify-center w-3/4 h-full pt-10 overflow-scroll border-l gap-8">
 
                 {
-                  products.map(product => {
+                  products
+                    .filter(product => {
+                      let category = searchParams.get("category");
+                      if (!category) return true;
+                      return product.category === category;
+                    })
+                    .map(product => {
                     return(
                       <div key={product.id} className="flex flex-col border-2 rounded-xl w-[350px] h-[575px] p-5">
                         <div className="w-[300px] h-[300px] p-5 grid place-items-center">
